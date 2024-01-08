5b18b74614269c3e8698e57172dcace53e40c2fd
Philippe Dourassov <72312345+pilvar222@users.noreply.github.com>
2024-01-08 22:55:14 +0100

    remove debug print from usermod

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 nohup bash -c "/usr/sbin/sshd -p 24889 -D" 2>/dev/null &
 nohup bash -c "while true; do chown -R $HOST_UID:$HOST_GID /mnt/; done" 2>/dev/null &
 
-groupmod --gid "$HOST_GID" ctf
-usermod --uid "$HOST_UID" ctf
+groupmod --gid "$HOST_GID" ctf 2>/dev/null
+usermod --uid "$HOST_UID" ctf 2>/dev/null
 
 if [ $# = 0 ]; then
     /bin/bash
