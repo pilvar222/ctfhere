f0893449288b183709d91199910e95e65ca2a5e0
Philippe Dourassov <philippe.dourassov@epfl.ch>
2024-01-08 23:36:45 +0100

    build.sh: fix bad escaping for bashrc alias "ctfhere"

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ docker build -t ctf .
 rm ./id_rsa.pub
 sed -i '/alias alsoctf/d' ~/.bashrc
 sed -i '/alias ctfhere/d' ~/.bashrc
-echo "alias ctfhere=\"echo \\\"\$(id -u) \$(id -g) \$(pwd)\\\" | xargs printf -- 'docker run -p24889:24889 -it --name ctf -e HOST_UID=%s -e HOST_GID=%s --rm -v %s:/mnt ctf:latest /bin/bash' | xargs xargs -o\"" >> ~/.bashrc
+echo "alias ctfhere=\"echo \\\"\\\$(id -u) \\\$(id -g) \\\$(pwd)\\\" | xargs printf -- 'docker run -p24889:24889 -it --name ctf -e HOST_UID=%s -e HOST_GID=%s --rm -v %s:/mnt ctf:latest /bin/bash' | xargs xargs -o\"" >> ~/.bashrc
 echo "alias alsoctf=\"docker exec -it ctf /bin/bash\"" >> ~/.bashrc
 source ~/.bashrc
