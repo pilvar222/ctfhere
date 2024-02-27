762bbacf6bb2b571056a126400508670709d40e1
Philippe Dourassov <philippe.dourassov@epfl.ch>
2024-02-27 15:40:10 +0200

    hacky patch for challs with relative imports

--- pwninitTemplate.py
+++ pwninitTemplate.py
@@ -21,9 +21,10 @@ def conn():
     if args.GDB:
       os.system('ssh-keygen -f "~/.ssh/known_hosts" -R "[localhost]:24889"')
       s = ssh(user="root", host='localhost', port=24889)
+      s.set_working_directory(''.join(exe.path.split("/")[:-1])) # hacky way to fix challenges with relative imports. TODO: make something cleaner that this with a pwninit patch
       r = s.process(["/mnt/"+exe.path.split("/")[-1]])
       gdb.attach(r)
-    else if is_docker() or args.DANGER:
+    elif is_docker() or args.DANGER:
       r = process({proc_args})
     else:
       error("You are not running from a container. Please use REMOTE, GDB, or DANGER argument. (GDB and REMOTE are safe. DANGER will run the binary on your host machine.)")
