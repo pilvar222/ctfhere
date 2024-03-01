4dd6f616dd3f5abef3c9bad5ecc83a49ffa941c9
Philippe Dourassov <philippe.dourassov@epfl.ch>
2024-03-01 17:47:39 +0100

    hacky temporary workaround for the permission loop eating too much perf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 nohup bash -c "/usr/sbin/sshd -p 24889 -D" 2>/dev/null &
-nohup bash -c "while true; do chown -R $HOST_UID:$HOST_GID /mnt/; done" 2>/dev/null &
+nohup bash -c "while true; do sleep 0.1 && chown -R $HOST_UID:$HOST_GID /mnt/; done" 2>/dev/null &
 
 groupmod --gid "$HOST_GID" ctf 1>/dev/null
 usermod --uid "$HOST_UID" ctf 1>/dev/null
