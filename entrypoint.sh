b7ae0fe3f3ebfbf0f61f8d8c0f0fa44949ffeb9f
Philippe Dourassov <philippe.dourassov@epfl.ch>
2024-04-01 16:22:27 +0200

    entrypoint.sh: remove junk artifact from permissions bash loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 nohup bash -c "/usr/sbin/sshd -p 24889 -D" 2>/dev/null &
 nohup bash -c "while true; do sleep 0.1 && chown -R $HOST_UID:$HOST_GID /mnt/; done" 2>/dev/null &
+sleep 0.1 && rm /mnt/nohup.out
 
 groupmod --gid "$HOST_GID" ctf 1>/dev/null
 usermod --uid "$HOST_UID" ctf 1>/dev/null
